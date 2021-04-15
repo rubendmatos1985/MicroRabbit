69ef20b9d145cd07e6355e0d3893a9f2aab5e50a
rubendmatos1985 <rubendmatos1985@gmail.com>
2021-04-15 18:49:07 +0200

    MicroRabbit Banking Domain Models first commit

---
+++ MicroRabbit.Banking.Domain/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ * there is any security implementation 
+ * maybe in the future
+ * that is just an example of how to use microservices
+ */
+namespace MicroRabbit.Banking.Domain.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string AccountType { get; set; }
+
+        public decimal AccountBalance { get; set; }
+    }
+}
