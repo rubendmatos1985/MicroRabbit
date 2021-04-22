9c4cfa5d5fab2f23be99204de1b87ef153e04823
rubendmatos1985 <rubendmatos1985@gmail.com>
2021-04-22 16:19:30 +0200

    banking db context ready

--- MicroRabbit.Banking.Data/Context/BankingDbContext.cs
+++ MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -6,9 +6,7 @@ namespace MicroRabbit.Banking.Data.Context
     public class BankingDbContext : DbContext
     {
         public BankingDbContext(DbContextOptions options) : base(options)
-        {
-
-        }
+        {}
 
         public DbSet<Account> Accounts { get; set; }
     }
